adad50e24590ce0e420719ee1f3bf42f50a6b1cc
Pinkhat <mahdis.ghaffarian@gmail.com>
2021-01-18 12:16:39 +0100

    cloud

---
+++ cloud-spark/setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+### Main variables
+
+# User ID on GCP
+export GCP_userID="ghaffarian_mahdis_gmail_com"
+
+# Private key to use to connect to GCP
+export GCP_privateKeyFile="~/.ssh/id_ed25519"
+
+# Name of your GCP project
+export TF_VAR_project="spark-project-302109"
+
+# Name of your selected GCP region
+export TF_VAR_region="us-central1"
+
+# Name of your selected GCP zone
+export TF_VAR_zone="us-central1-b"
+
+
+
+### Other variables used by Terrform
+
+# Number of VMs created
+export TF_VAR_machineCount=1
+
+# VM type
+export TF_VAR_machineType="n2-highmem-8"
+
+# Prefix for you VM instances
+export TF_VAR_instanceName="tf-instance"
+
+# Prefix of your GCP deployment key
+export TF_VAR_deployKeyName="spark-service-project.json"
---
+++ cloud-spark/variables.tf
@@ -0,0 +1,29 @@
+variable "project" {
+  default = "default_value"
+}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-b"
+}
+
+variable "instanceName" {
+  default = "terraform-instance"
+}
+
+variable "deployKeyName" {
+  default = "spark-service-project.json"
+}
+
+variable "machineCount" {
+  default = 1
+}
+
+variable "machineType" {
+  default = "n2-highmem-8"
+}
+
+
